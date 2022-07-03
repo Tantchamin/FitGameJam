da452c3d862cc8df03cd7be4851e68383c1e932d
Tantchamin <78613005+Tantchamin@users.noreply.github.com>
2022-07-03 14:31:45 +0700

    Update Scene add scripts

---
+++ GameJamProject/Assets/Scripts/GalleryScripts/ItemInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemInfo : MonoBehaviour
+{
+    public string name = "_";
+    public string info = "_";
+    public Sprite image;
+}
